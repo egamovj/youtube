795241a9b2b2a91f2ce012fab068cabc67ae2838
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2023-12-16 23:50:45 +0500

    videos are change

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,7 +4,7 @@ export { default as Search } from './Search/Search';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as SearchBar } from './SearchBar/SearchBar';
 export { default as Category } from './Category/Category';
-export { default as Videos } from './videos/videos';
+export { default as Videos } from './Videos/Videos';
 export { default as VideoCard } from './VideoCard/VideoCard';
 export { default as ChannelCard } from './ChannelCard/ChannelCard';
 export { default as Loader } from './Loader/Loader';
